a12112d10006073f1476b11c1a01b30ce57038bd
KC Maddever <jickerjatt@gmail.com>
2018-12-22 21:20:32 +1300

    Add framework for plugin

---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: vc-perpost
+# about: Plugin for Discourse to show the latest vote made by each user as of that post (for Mafia451)
+# version: 0.0.1
+# authors: KC Maddever (Jickerjatt)
+# url: https://github.com/Jickerjatt/vc-perpost
